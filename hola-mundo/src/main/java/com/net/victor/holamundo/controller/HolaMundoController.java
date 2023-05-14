610ca95c36ff5433d8e2abe893a600aa0d22ee39
Alex <victor16alex@gmail.com>
2023-05-14 00:37:44 -0600

    First commit

---
+++ hola-mundo/src/main/java/com/net/victor/holamundo/controller/HolaMundoController.java
@@ -0,0 +1,28 @@
+package com.net.victor.holamundo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hola-mundo")
+public class HolaMundoController {
+
+    @GetMapping("/saludar")
+    @RequestMapping(vA)
+    public String saludar() {
+        return null;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
